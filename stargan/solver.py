10836dcd6692ea87d44fbd8703ad4e336af60f24
Nataniel Ruiz <nruiz@Nataniels-MacBook-Pro.local>
2019-12-24 19:59:51 -0400

    next

--- stargan/solver.py
+++ stargan/solver.py
@@ -594,10 +594,10 @@ class Solver(object):
 
         for i, (x_real, c_org) in enumerate(data_loader):
             # Black image
-            black = np.zeros((1,3,256,256))
-            black = torch.FloatTensor(black).to(self.device)
+            # black = np.zeros((1,3,256,256))
+            # black = torch.FloatTensor(black).to(self.device)
 
-            # black = torch.FloatTensor(torch.rand((1,3,256,256))).to(self.device)
+            black = torch.FloatTensor(torch.rand((1,3,256,256))).to(self.device)
 
             # Prepare input images and target domain labels.
             x_real = x_real.to(self.device)
